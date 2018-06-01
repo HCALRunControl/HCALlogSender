597908d87a41ab22cc781998e365a20ccc58ac9e
jhakala <john_hakala@brown.edu>
2018-06-01 21:06:04 +0200

    improve style

--- src/HCALlogSender.java
+++ src/HCALlogSender.java
@@ -4,40 +4,20 @@ import org.apache.log4j.PropertyConfigurator;
 import rcms.util.logger.RCMSLogger; // reuse the RCMS implementation for log4j
 
 /**
- * Simple script to send messages to the logCollector 
- * to build, go on a teststand machine with RCMS installed:
- *   -------
- *   daqowner:~ $ cd /home/daqowner/TriDAS
- *   daqowner:~/TriDAS $ git clone git@github.com:HCALRunControl/HCALlogSender.git
- *   daqowner:~/TriDAS $ cd HCALlogSender
- *   ant
- *   -------
- * the compiled library will show up in the `jars` subdirectory
- *
- * to run, copy ~daqowner/TriDAS/jars/HCALlogSender.jar to your home directory on cmsusr
- * it needs to be run, on a machine on the cmsusr network with java1.8 installed (e.g. x2go01).
- * then to send e.g. the message "this is a test error!" to the logCollector logs:
- *   -------
- *   myusername@x2go01 ~ # java -jar HCALlogSender.jar 'this is a test error!'
- *   -------
- */
-
-
-/**
- * @author johakala
+ * @author John Hakala
  * 6/1/2018
  * standalone class for sending arbitrary log messages to the logs
+ * see README.md for instructions for building and running
  */
+
 public class HCALlogSender {
   RCMSLogger logger;
   /**
    * constructor to set up the log sender, specifying what application name will appear next to the log message
    */
   public HCALlogSender(String name) {
-    // TODO Auto-generated constructor stub
-    this.logger=new RCMSLogger(name);
+    this.logger = new RCMSLogger(name);
   }
-
 
   /**
    * main method of the class -- gets called when you invoke this program from the command line
